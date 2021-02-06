07bf0157ab0f4c77d811809aa06c2c5f3a17a70a
Doniben <doniben@esperanto.co>
2021-02-05 20:17:34 -0500

    [FEATURE] routes added for each Triario-task

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,11 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('./database')
 
 // Setting Server
 app.set('port', 4000)
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 // app.use(bodyParser.urlencoded({extended:true}))
 // app.use(bodyParser.json())
 
@@ -15,3 +16,10 @@ app.set('port', 4000)
 app.listen(app.get('port'), () => {
     console.log('Listening on port', app.get('port'));
 })
+
+// Routes
+
+app.use('/api/cardvalidator', require('./routes/cardValidator.route'))
+app.use('/api/employees', require('./routes/employee.route'))
+app.use('/api/perfect', require('./routes/perfectNumber.route'))
+app.use('/api/even', require('./routes/evenNumber.route'))
